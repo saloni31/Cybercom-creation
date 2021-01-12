067e72f0c996e84d9bddf17b3c34d4301fa7c18b
Saloni Sindhi <sindhisaloni31@gmail.com>
2021-01-12 12:52:12 +0530

    complete array function.

--- 11-01-2021/js/booleanExpression.js
+++ 11-01-2021/js/booleanExpression.js
@@ -25,3 +25,35 @@ age >= 18 ? console.log(firstName + " drinks beer"): console.log(firstName + " d
 
 var drink = age >= 18 ? 'beer' : 'juice';
 console.log(drink);
+
+
+/****************
+Switch statement
+*****/
+var job='teacher';
+switch(job){
+	case 'teacher':
+	case 'instructor':
+		console.log(firstName + ' teaches a code.');
+		break;
+	case 'driver':
+		console.log(firstName + ' drives a car.');
+		break;
+	default:
+		console.log(firstName + ' do something else.');
+}
+
+
+switch(true){
+	case age < 13:
+		console.log(firstName + " is student");
+		break;
+	case age >= 13 && age < 20:
+		console.log(firstName + " is teenager");
+		break;
+	case age >= 20 && age < 30:
+		console.log(firstName + " is Younger");
+		break;
+	default:
+		console.log(firstName + " is elder person");
+}
